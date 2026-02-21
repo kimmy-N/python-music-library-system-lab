a507538401d503d2ee5a38c06cd47a1a3d29e834
kimmy-N <ayiakinashipai@gmail.com>
2026-02-21 14:42:38 +0300

    Fixed Song class

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,34 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # increase total song count
+        Song.count += 1
+
+        # store unique genres
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+
+        # store unique artists
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+
+        # count songs per genre
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        # count songs per artist
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
